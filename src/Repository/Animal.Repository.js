6fcc12aaced96ed1c4b8296ce440dbaafd323de8
andz1308 <daianh2k00@gmail.com>
2025-07-04 18:16:40 +0700

    finish

--- src/Repository/Animal.Repository.js
+++ src/Repository/Animal.Repository.js
@@ -1,11 +1,28 @@
 const Animal = require('../Models/Animal.Model');
+const Counter = require('../Models/Counter.Model');
 
 const AnimalRepository = {
-  create: async (data) => await Animal.create(data),
-  findAll: async () => await Animal.find().populate('farmId'),
-  findById: async (id) => await Animal.findById(id).populate('farmId'),
-  update: async (id, data) => await Animal.findByIdAndUpdate(id, data, { new: true }),
-  delete: async (id) => await Animal.findByIdAndDelete(id),
+  create: async (data) => {
+    // Tạo animal mới, farmId sẽ tự tăng nếu model đã có hook pre-save
+    const animal = new Animal(data);
+    return await animal.save();
+  },
+
+  findAll: async () => {
+    return await Animal.find();
+  },
+
+  findById: async (id) => {
+    return await Animal.findOne({ farmId: Number(id) });
+  },
+
+  update: async (id, data) => {
+    return await Animal.findOneAndUpdate({ farmId: Number(id) }, data, { new: true });
+  },
+
+  delete: async (id) => {
+    return await Animal.findOneAndDelete({ farmId: Number(id) });
+  },
 };
 
 module.exports = AnimalRepository;
--- src/Services/Animal.Service.js
+++ src/Services/Animal.Service.js
@@ -1,11 +1,26 @@
-const AnimalRepository = require('../Repository/Animal.Repository');
+const Animal = require('../Models/Animal.Model');
 
 const AnimalService = {
-  createAnimal: async (data) => await AnimalRepository.create(data),
-  getAllAnimals: async () => await AnimalRepository.findAll(),
-  getAnimalById: async (id) => await AnimalRepository.findById(id),
-  updateAnimal: async (id, data) => await AnimalRepository.update(id, data),
-  deleteAnimal: async (id) => await AnimalRepository.delete(id),
+  createAnimal: async (data) => {
+    const animal = new Animal(data);
+    return await animal.save();
+  },
+
+  getAllAnimals: async () => {
+    return await Animal.find();
+  },
+
+  getAnimalById: async (id) => {
+    return await Animal.findOne({ farmId: Number(id) });
+  },
+
+  updateAnimal: async (id, data) => {
+    return await Animal.findOneAndUpdate({ farmId: Number(id) }, data, { new: true });
+  },
+
+  deleteAnimal: async (id) => {
+    return await Animal.findOneAndDelete({ farmId: Number(id) });
+  },
 };
 
 module.exports = AnimalService;
