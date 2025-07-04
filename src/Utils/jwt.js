07bbd3b9aa21640ceeccc3bfb1e68558216fbe02
andz1308 <daianh2k00@gmail.com>
2025-07-04 12:29:40 +0700

    update

--- src/Utils/jwt.js
+++ src/Utils/jwt.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+const JWT_SECRET = process.env.JWT_SECRET || 'f3b7d1a6c2e4f8b5a0d9c7e1f4a8b3d';
 
-function signToken(payload) {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
-}
+const signToken = (payload, expiresIn = '1d') => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn });
+};
 
-function verifyToken(token) {
+const verifyToken = (token) => {
   return jwt.verify(token, JWT_SECRET);
-}
+};
 
 module.exports = { signToken, verifyToken };
